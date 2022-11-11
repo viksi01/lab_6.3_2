567ec52019b7a8ad0d276c0f0cc951e164b736d9
viksi01 <victoria.pyatnicka@gmail.com>
2022-11-11 11:03:34 +0200

    first commit

---
+++ UnitTest_6.3_2/UnitTest_6.3_2.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_6.3_2/lab_6.3_2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest632
+{
+	TEST_CLASS(UnitTest632)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int a[5] = { -50, - 41, 17, 61, 10 };
+			int t = Number(a, 5, 1,1);
+			Assert::AreEqual(t, 4);
+
+		}
+	};
+}
---
+++ lab_6.3_2/lab_6.3_2.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+
+void Show(int* a, const int n, int i)
+{
+    a[i] = -100 + rand() % (-100 - 100) + 1;
+    if (i < n - 1)
+        Show(a, n, i + 1);
+}
+
+void Print(int* a, const int n, int i)
+{
+    cout << a[i] << " ";
+    if (i < n - 1)
+        Print(a, n, i + 1);
+    else
+        cout << endl;
+}
+int Number(int* a, const int n, int i, int num)
+{
+    if (a[i] > 0)
+    num += 1;
+    if (i < n)
+    {
+        return Number(a, n, i + 1, num);
+    }
+    else {
+        return num;
+    }
+}
+template <typename T>
+void ShowT (T* a, const int n, int i)
+{
+    a[i] = -100 + rand() % (-100 - 100) + 1;
+    if (i < n - 1)
+        Show(a, n, i + 1);
+}
+
+template <typename T>
+void PrintT (T* a, const int n, int i)
+{
+    cout << a[i] << " ";
+    if (i < n - 1)
+        Print(a, n, i + 1);
+    else
+        cout << endl;
+}
+
+template <typename T>
+int NumberT (T* a, const int n, int i, int num)
+{
+    if (a[i] > 0)
+        num += 1;
+    if (i < n)
+    {
+        return Number(a, n, i + 1, num);
+    }
+    else {
+        return num;
+    }
+}
+int main()
+{
+    srand((unsigned)time(NULL));
+    const int n = 20;
+    int a[n];
+    Show(a, n, 0);
+    cout << "a {"; Print(a, n, 0); cout << "}" << endl;
+    cout << "Number of positive = " << Number(a, n, 0, 0) << endl;
+
+    ShowT(a, n, 0);
+    cout << "a {"; PrintT(a, n, 0); cout << "}" << endl;
+    cout << "Number of positive T = " << Number(a, n, 0, 0);
+}
